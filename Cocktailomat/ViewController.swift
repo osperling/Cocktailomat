f9f62803b6f9435040cfab2dbc1fee5e1f9b5028
osperling <osperling@users.noreply.github.com>
2020-10-11 21:44:56 +0200

    Initial Commit

---
+++ Cocktailomat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cocktailomat
+//
+//  Created by Oliver Sperling on 11.10.20.
+//  Copyright © 2020 Oliver Sperling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
